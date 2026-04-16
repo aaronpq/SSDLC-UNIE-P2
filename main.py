3c22167756ae467fa504b9c62ea9ff9b3927ed30
aaronpq <aperezq@student.universidadunie.com>
2026-04-17 00:24:28 +0200

    Corrección - Modo debug activo en producción

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from server import app
 from routes import auth, companies, companies_admin, users_admin, profile
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=False, use_reloader=False)
