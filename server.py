c9d2b38ddd9c60c827418e6023b15187308530c8
aaronpq <aperezq@student.universidadunie.com>
2026-04-17 20:16:13 +0200

    Corrección - Reutilización de sesión post-logout

--- server.py
+++ server.py
@@ -2,16 +2,19 @@ import os
 from flask import Flask, render_template
 from flask_wtf.csrf import CSRFProtect
 from datetime import timedelta
+from flask_session import Session  # <--- 1. Importar la extensión
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.config.update(
     SESSION_COOKIE_HTTPONLY=True,
     SESSION_COOKIE_SAMESITE='Lax',
-    SESSION_COOKIE_SECURE=False 
+    SESSION_COOKIE_SECURE=False,
+    SESSION_TYPE='filesystem',      # <--- 2. Guardar sesiones en archivos (servidor)
+    SESSION_PERMANENT=True
 )
+Session(app)
 csrf = CSRFProtect(app)
-#app.permanent_session_lifetime = 99999999
 app.permanent_session_lifetime = timedelta(minutes=30)
 
 @app.errorhandler(404)
