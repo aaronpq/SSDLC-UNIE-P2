631b6c4d4123a1d37bfc665593bb4a5d957a96d2
aaronpq <aperezq@student.universidadunie.com>
2026-04-18 11:43:45 +0200

    Corrección - Modo debug activo en producción version2

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template
 from flask_wtf.csrf import CSRFProtect
 from datetime import timedelta
-from flask_session import Session  # <--- 1. Importar la extensión
+from flask_session import Session
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -10,8 +10,8 @@ app.config.update(
     SESSION_COOKIE_HTTPONLY=True,
     SESSION_COOKIE_SAMESITE='Lax',
     SESSION_COOKIE_SECURE=False,
-    SESSION_TYPE='filesystem',      # <--- 2. Guardar sesiones en archivos (servidor)
-    SESSION_PERMANENT=True
+    SESSION_TYPE='filesystem',
+    SESSION_PERMANENT=False
 )
 Session(app)
 csrf = CSRFProtect(app)
@@ -25,3 +25,6 @@ def not_found(e):
 def forbidden(e):
     return render_template('errors/403.html'), 403
 
+@app.errorhandler(500)
+def not_found(e):
+    return render_template('errors/500.html'), 500
